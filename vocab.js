efe7d8fa70c0a0e8a9bf6e9d7f71df7c9a3ebc58
David Robbins <robbins.david@gmail.com>
2015-08-31 15:28:39 -0500

    First draft of angular version

--- vocab.js
+++ vocab.js
@@ -12,122 +12,61 @@
 
     let v = window.v = {}
 
-    class VocabularySelect {
-        constructor (selector, dimension, group) {
-            this.$el = $(selector);
-            this.d = dimension;
-            this.g = group;
-            this.loadOptions();
-        }
+    let app = angular.module("vocabularyFilter", ["datatables", "ngSanitize", "ui.select"]);
 
-        selectItemMap (item) {
-            return {
-                id: item.key,
-                text: item.key
-            };
-        }
-
-        loadOptions () {
-            this.$el.val(this.val).select2({
-                data: this.g.all().map(this.selectItemMap)
-            })
-        }
-
-        refresh () {
-            this.val = this.$el.val();
-            this.d.filterExact(this.val);
-            this.$el.empty();
-            this.loadOptions();
-        }
-
-        addListener (evt, listener) {
-            this.$el.on(evt, listener)
-        }
-
-        get $() {
-            return this.$el;
-        }
-    }
-
-    class VocabularyTable {
-        constructor (selector, dimension){
-            this.$el = $(selector);
-            this.d = dimension;
-            this.refresh();
-        }
-
-        refresh () {
-            this.$el.DataTable({
-                data: this.d.top(Infinity),
-                columns: displayFields.map(function (field) {
-                    return {
-                        data: field,
-                        title: field
-                    }
-                }),
-                destroy: true
+    app.service("vcf", ["$q", "$window", function ($q, $window) {
+        let d3 = $window.d3;
+        //return function () {
+            let defer = $q.defer();
+            // Load the Datas
+            d3.json(vocabUrl, function (err, data) {
+                if (err) {
+                    defer.fail(err);
+                } else {
+                    v.raw = data;
+                    // Setup Crossfilters
+                    v.cf = crossfilter(data);
+                    v.all = v.cf.groupAll();
+                    v.d = {
+                        byCategory: v.cf.dimension(function (d) { return d["Category"]; }),
+                        byAnatomicSite: v.cf.dimension(function (d) { return d["Anatomic Site"]; }),
+                        bySubsite: v.cf.dimension(function (d) { return d["Subsite"]; }),
+                        byDiagnosis: v.cf.dimension(function (d) { return d["Diagnosis"]; }),
+                        byDiagnosisModifier: v.cf.dimension(function (d) { return d["Diagnosis Modifier"]; }),
+                    };
+                    v.g = {
+                        groupByCategory: v.d.byCategory.group(),
+                        groupByAnatomicSite: v.d.byAnatomicSite.group(),
+                        groupBySubsite: v.d.bySubsite.group(),
+                        groupByDiagnosis: v.d.byDiagnosis.group(),
+                        groupByDiagnosisModifier: v.d.byDiagnosisModifier.group(),
+                    };
+                    defer.resolve({
+                        cf: v.cf,
+                        d: v.d,
+                        g: v.g
+                    });
+                }
             });
-        }
+            return defer.promise;
+        //};
+    }]);
 
-        addListener () {}
-    }
+    app.controller("VocabularFilterController", ["$scope", "vcf", function ($scope, vcf){
+        vcf.then(function (data) {
+            angular.extend($scope, data);
+        });
+    }]);
 
-    class VocabularyFilterUpdater {
-        constructor (elements) {
-            let that = this;
-            this.elements = elements;
-            this.elements.forEach(function (element) {
-                element.addListener("change", that.refresh.bind(that));
-            });
-        }
-
-        refresh () {
-            this.elements.forEach(function (element) {
-                element.refresh();
-            });
-        }
-    }
-
-
-    // Load the Datas
-    d3.json(vocabUrl, function (err, data) {
-
-        v.raw = data;
-
-        // Setup Crossfilters
-        v.cf = crossfilter(data);
-        v.all = v.cf.groupAll();
-        v.d = {
-            byCategory: v.cf.dimension(function (d) { return d["Category"]; }),
-            byAnatomicSite: v.cf.dimension(function (d) { return d["Anatomic Site"]; }),
-            bySubsite: v.cf.dimension(function (d) { return d["Subsite"]; }),
-            byDiagnosis: v.cf.dimension(function (d) { return d["Diagnosis"]; }),
-            byDiagnosisModifier: v.cf.dimension(function (d) { return d["Diagnosis Modifier"]; }),
+    app.directive("chtnvs", function () {
+        return {
+            templateUrl: "templates/chtnVocabularySelect.html",
+            scope: {
+                dimension: "=dimension",
+                group: "=group",
+                label: "@label"
+            }
         };
-        v.g = {
-            groupByCategory: v.d.byCategory.group(),
-            groupByAnatomicSite: v.d.byAnatomicSite.group(),
-            groupBySubsite: v.d.bySubsite.group(),
-            groupByDiagnosis: v.d.byDiagnosis.group(),
-            groupByDiagnosisModifier: v.d.byDiagnosisModifier.group(),
-        };
-
-        let vocabularyTable = new VocabularyTable(".vocab-table", v.d.byCategory);
-        let categorySelect = new VocabularySelect("#category", v.d.byCategory, v.g.groupByCategory);
-        let anatomicSiteSelect = new VocabularySelect("#anatomicSite", v.d.byAnatomicSite, v.g.groupByAnatomicSite);
-        let subsiteSelect = new VocabularySelect("#subsite", v.d.bySubsite, v.g.groupBySubsite);
-        let diagnosisSelect = new VocabularySelect("#diagnosis", v.d.byDiagnosis, v.g.groupByDiagnosis);
-        let diagnosisModifierSelect = new VocabularySelect("#diagnosisModifier", v.d.byDiagnosisModifier, v.g.groupByDiagnosisModifier);
-
-        v.vfu = new VocabularyFilterUpdater([
-            vocabularyTable,
-            categorySelect,
-            anatomicSiteSelect,
-            subsiteSelect,
-            diagnosisSelect,
-            diagnosisModifierSelect
-        ]);
-
     });
 
 })();
